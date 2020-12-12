76185229b07116bb60a45521d69c7bf1b2a3b606
Gergo06 <58563706+Gergo06-py@users.noreply.github.com>
2020-12-12 09:06:58 +0100

    Update notifier_libraries.py

--- notifier_libraries.py
+++ notifier_libraries.py
@@ -3,6 +3,7 @@ classes = [["Matematika", "Testnevelés", "Kémia", "Kémia", "Irodalom", "Angol
            ["", "Osztályfőnöki", "Fizika", "Matematika", "Történelem", "Irodalom", "Testnevelés", "Testnevelés"],
            ["", "Testnevelés", "Irodalom", "Angol nyelv", "Matematika", "Magyar nyelv", "Fizika"],
            ["Testnevelés", "Történelem", "Történelem", "Angol nyelv"],
-           ["Osztályfőnöki", "Osztályfőnöki", "Osztályfőnöki", "Osztályfőnöki"]]
+           ["Osztályfőnöki", "Osztályfőnöki", "Osztályfőnöki", "Osztályfőnöki"],
+           []]
 
 time_table = [["8:00", "8:45"], ["8:55", "9:40"], ["9:55", "10:40"], ["10:50", "11:35"], ["11:45", "12:30"], ["12:50", "13:35"], ["13:45", "14:30"], ["14:35", "15:15"], ["15:20", "16:00"]]
